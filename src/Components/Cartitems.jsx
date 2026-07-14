a5b862225e90a3133c28ec3ca3e6621d418b8f8f
Pavan-Raj-04 <praj0412k2@gmail.com>
2026-07-14 16:03:56 +0530

    Added table to cart page to hold the cart added items data and display number of items added and total price of added items

--- src/Components/Cartitems.jsx
+++ src/Components/Cartitems.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Cartitems = () => {
+
+
+  let [apiData, setApiData] = useState([])
+
+  let fetchData = async()=>{
+    let response = await axios.get('http://localhost:4000/cartitems')
+    console.log(response.data)
+    setApiData(response.data)
+
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+  let total = apiData.reduce((acc,elem)=>{
+    return acc + Math.floor((elem.price)*95)
+  }, 0)
+
   return (
-    <div>
-        <h1>cart component</h1>
-    </div>
+    <>
+      <div className="cart-items">
+        <div className="table">
+          <table border={1}>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Catrgory</th>
+                <th>Rating</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {apiData.map((elem,index)=>{
+                let {id,title, price, description, category, image, rating} = elem
+                return(
+                  <>
+                    <tr key={index}>
+                      <td>{index+1}</td>
+                      <td>{title}</td>
+                      <td>{category}</td>
+                      <td>{rating?.rate}</td>
+                      <td>{Math.floor(price*95)}</td>
+                    </tr>
+                  </>
+                )
+              })}
+    
+            </tbody>
+          </table>
+          <h2>{`Subtotal (${apiData.length} items): ${total}`}</h2>
+        </div>
+      </div>        
+    </>
   )
 }
 
